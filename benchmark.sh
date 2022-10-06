10c5a67ae61393031d5327afb29d29344eedad15
Bailey <lissington4@gmail.com>
2022-10-06 22:41:09 +1300

    updated benchmark to perform 300 iterations

--- benchmark.sh
+++ benchmark.sh
@@ -2,6 +2,6 @@
 
 hyperfine --export-json benchmark.json \
 --parameter-scan num_threads 1 7 \
-'OMP_NUM_THREADS={num_threads} ./poisson -n 51 -i 30'
+'OMP_NUM_THREADS={num_threads} ./poisson -n 51 -i 300'
 
 python benchmark_plot.py benchmark.json -o benchmark.png
