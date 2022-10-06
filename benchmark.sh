6aee327a1c1d6cd03748b151a25a30b8fa6940fe
Bailey <lissington4@gmail.com>
2022-10-06 21:05:43 +1300

    removed venv activation from benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -2,8 +2,6 @@
 
 hyperfine --export-json benchmark.json \
 --parameter-scan num_threads 1 7 \
-'OMP_NUM_THREADS={num_threads} ./poisson -n 301 -i 30'
-
-source venv/bin/activate
+'OMP_NUM_THREADS={num_threads} ./poisson -n 51 -i 30'
 
 python benchmark_plot.py benchmark.json -o benchmark.png
