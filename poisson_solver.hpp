61ac4c19ce588c8115a519730651d5729fafad01
Bailey <lissington4@gmail.com>
2022-10-13 13:07:56 +1300

    Added comments

--- poisson_solver.hpp
+++ poisson_solver.hpp
@@ -3,6 +3,9 @@
 
 #include <vector>
 
+/**
+ * @brief Class for solving Poisson's equation using Jacobi Relaxation
+ */
 class PoissonSolver
 {
 private:
@@ -16,6 +19,16 @@ private:
     std::vector<float> *next;
 
 public:
+    /**
+     * @brief Construct a new Poisson Solver object
+     *
+     * @param n: Size of cube (nxnxn)
+     * @param source:  Source field volume
+     * @param iterations: Number of iterations of the Jacobi Relaxation
+     * @param threads: Number of threads to perform computation on
+     * @param delta: Distance between volume voxels
+     * @param debug: Whether debugging data is printed to stdout
+     */
     PoissonSolver(int n,
                   const std::vector<float> &source,
                   int iterations,
@@ -23,6 +36,10 @@ public:
                   float delta,
                   bool debug);
 
+    /**
+     * @brief Solves Poisson's equation using Jacobi Relaxation
+     * @return Field after the specified number of iterations
+     */
     std::vector<float> *solve(void);
 };
 
